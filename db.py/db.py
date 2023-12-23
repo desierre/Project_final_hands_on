3e015a88c1fd3f95349d592519c82a3f157a4e48
desierre <152476820+desierre@users.noreply.github.com>
2023-12-23 22:12:03 +0800

    Add files via upload

---
+++ db.py/db.py
@@ -0,0 +1,7 @@
+class Person(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    age = db.Column(db.Integer, nullable=False)
+    city = db.Column(db.String(50), nullable=False)
+    
+    
